025ae4a046709eb84ccd6bbc5e9bed311a68c4f6
fmfahath <fmfahath.dev@gmail.com>
2023-12-20 05:04:35 +0530

    popup open/close css,function added+

---
+++ app.js
@@ -0,0 +1,9 @@
+let popupEl = document.getElementById('popup');
+
+function openPopup(){
+    popupEl.classList.add('open-popup');
+}
+
+function closePopup(){
+    popupEl.classList.remove('open-popup');
+}
